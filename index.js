829e6ff4e08dbddb0a90131d9068199a3755f452
Pedro Pires <pe8542@gmail.com>
2021-02-10 22:58:52 -0300

    Adding samples of handlers for some kinds of routes that the user may use and returns JSON

--- index.js
+++ index.js
@@ -38,12 +38,37 @@ var server = http.createServer(function(req, res) {
     req.on('end', function() {
         buffer += decoder.end();
 
-        // Send the response
-        res.end(' Hello World\n');
+        // Choose the handler this request should go to. If one is not found use the notFound handler
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // Log the request path
-        console.log('Request received with this payload: ', buffer);
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
 
+        // Route the request to the handler especify in the router
+        chosenHandler(data, function(statusCode, payload){
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
     });
 });
 
@@ -51,3 +76,22 @@ var server = http.createServer(function(req, res) {
 server.listen(3000, function() {
     console.log('The server is listening on port 3000 now');
 });
+
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback) {
+    // Callback a http status code, and a payload object
+    callback(406, {'name' : 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+// Define a request router
+var router = {
+    'sample' : handlers.sample
+};
