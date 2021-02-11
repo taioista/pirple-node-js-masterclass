7533506eef821b8b5ef27a74c6c44a040fcfbb4c
Pedro Pires <pe8542@gmail.com>
2021-02-10 21:19:33 -0300

    First commit of the REST API

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+* Primary file for the API
+*
+*
+*/
+
+
+//Dependencies
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
+//the server should respond to all requests with a string
+var server = http.createServer(function(req, res) {
+
+    // Get the url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end(' Hello World\n');
+
+        // Log the request path
+        console.log('Request received with this payload: ', buffer);
+
+    });
+});
+
+//Start the server, and have it listen to port 3000
+server.listen(3000, function() {
+    console.log('The server is listening on port 3000 now');
+});
