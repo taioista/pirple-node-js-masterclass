ac747aba97c9a80f8fa8009d7a260f71c2a086e4
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2021-02-13 23:47:21 -0300

    Adding the get, put and delete methods to the user module

--- api/lib/handlers.js
+++ api/lib/handlers.js
@@ -72,18 +72,103 @@ handlers._users.post = function(data, callback) {
 };
 
 // Users - get
+// Required data: phone
+// Optional data: none
+// @TODO only let an authenticated user access their object. Don't let them access anyone data.
 handlers._users.get = function(data, callback) {
-
+    // Check that the phone number is valid
+    var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone : false;
+    if(phone) {
+        // Lookup the user
+        _data.read('users', phone, function(err, data) {
+            if(!err && data) {
+                // Remove the hashed password from the user object before returning it to the requester
+                delete data.hashedPassword;
+                callback(200, data);
+            } else {
+                callback(404, {'Error' : 'Could not find the specified user'});
+            }
+        });
+    } else {
+        callback(400, {'Error' : 'Missing required field'});
+    }
 };
 
 // Users - put
+// Required data: phone
+// Optional data: firstName, lastName, password (at least one must be specified)
+// @TODO Only let an authenticated user update their own object. Don't let them update anyone else's
 handlers._users.put = function(data, callback) {
-
+    // Check for the required field
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone : false;
+    // Check for the optional fields
+    var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    
+    //Error if the phone is invalid
+    if(phone) {
+        // Error if nothing is sent to update
+        if(firstName || lastName || password) {
+            // Lookup the user
+            _data.read('users', phone, function(err, userData){
+                if(!err && data) {
+                    //Update the fields necessary
+                    if(firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if(lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if(password) {
+                        userData.hashedPassword = helpers.hash(password);
+                    }
+                    // Store the new updates
+                    _data.update('users', phone, userData, function(err) {
+                        if(!err) {
+                            callback(200);
+                        } else {
+                            console.log(err)
+                            callback(500, {'Error' : 'Could not update the user'});
+                        }
+                    });
+                } else {
+                    callback(404, {'Error' : 'The specified user does not exist'});
+                }
+            });
+        } else {
+            callback(400, {'Error' : 'Missing fields to update'});
+        }
+    } else {
+        callback(400, {'Error' : 'Missing required field'});
+    }
 };
 
 // Users - delete
+// Required field: phone
+// @TODO Only let an authenticated user delete their object. Dont let them delete anyone else
+// @TODO Cleanup (delete) any other data files associated with this user
 handlers._users.delete = function(data, callback) {
-
+    // Check that the phone number is valid
+    var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone : false;
+    if(phone) {
+        // Lookup the user
+        _data.read('users', phone, function(err, data) {
+            if(!err && data) {
+                _data.delete('users', phone, function(err) {
+                    if(!err) {
+                        callback(200);
+                    } else {
+                        callback(500, {'Error' : 'Could not delete the specified user'});
+                    }
+                });
+            } else {
+                callback(404);
+            }
+        });
+    } else {
+        callback(400, {'Error' : 'Missing required field'});
+    }
 };
 
 // Ping
