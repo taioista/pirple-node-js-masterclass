617bb440b9873c6334b0c2a73b4ae365320cd71c
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2021-02-14 20:20:15 -0300

    Adding the auth token service

--- api/index.js
+++ api/index.js
@@ -101,5 +101,6 @@ var unifiedServer = function(req, res) {
 // Define a request router
 var router = {
     'ping' : handlers.ping,
-    'users' : handlers.users
+    'users' : handlers.users,
+    'tokens' : handlers.tokens
 };
