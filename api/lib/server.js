bd9c75b80a19769d1df3ca8f45d5acccba9f9e45
Pedro Pires <pe8542@gmail.com>
2021-02-17 01:46:33 -0300

    Adding workers that will process the checks stored by the user. Refactor on the main class of the api

---
+++ api/lib/server.js
@@ -0,0 +1,119 @@
+/*
+* Server-related tasks
+*
+*/
+
+//Dependencies
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var config = require('./config');
+var StringDecoder = require('string_decoder').StringDecoder;
+var fs = require('fs');
+var handlers = require('./handlers');
+var helpers = require('./helpers');
+var path = require('path');
+
+// Instantiate the server module
+var server = {};
+
+// Instantiate the HTTP server
+server.httpServer = http.createServer(function(req, res) {
+    server.unifiedServer(req, res);
+});
+
+
+server.httpsServerOptions = {
+    'key' : fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+    'cert' : fs.readFileSync(path.join(__dirname, '/../https/cert.pem')) 
+};
+
+// Instantiate the HTTPS server
+server.httpsServer = https.createServer(server.httpsServerOptions, function(req, res) {
+    server.unifiedServer(req, res);
+});
+
+
+// All the server logic for both the http and https server
+server.unifiedServer = function(req, res) {
+    // Get the url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // Choose the handler this request should go to. If one is not found use the notFound handler
+        var chosenHandler = typeof(server.router[trimmedPath]) !== 'undefined' ? server.router[trimmedPath] : handlers.notFound;
+
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : helpers.parseJsonToObject(buffer)
+        };
+
+        // Route the request to the handler especify in the router
+        chosenHandler(data, function(statusCode, payload){
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
+    });
+};
+
+// Define a request router
+server.router = {
+    'ping' : handlers.ping,
+    'users' : handlers.users,
+    'tokens' : handlers.tokens,
+    'checks' : handlers.checks
+};
+
+server.init = function() {
+    // Start the HTTP server
+    server.httpServer.listen(config.httpPort, function() {
+        console.log('The server is listening on port ' +config.httpPort);
+    });
+
+    // Start the HTTPS server
+    server.httpsServer.listen(config.httpsPort, function() {
+        console.log('The server is listening on port ' +config.httpsPort);
+    });
+};    
+
+// Exports the module
+module.exports = server;
